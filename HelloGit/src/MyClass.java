4e14944125beb95c57f1872a80d902abcb86986a
yerim08 <sbnlover1008@naver.com>
2022-06-11 18:49:03 +0900

    first commit in dev branch

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -3,5 +3,7 @@ public class MyClass {
 	public static void main(String args[]) {
 		System.out.println("Hello Git!");
 		System.out.println("This is ??? version!");
+		MyHello myHello = new MyHello();
+		myHello.sayHello();
 	}
 }
